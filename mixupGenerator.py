c1def3c07bf9130221c0dbca90d99961ec74d9f3
janalexandersson <janalexandersson97@gmail.com>
2020-05-01 16:58:02 +0200

    Added mixup and plot function, fit_generator does not work for some reason

---
+++ mixupGenerator.py
@@ -0,0 +1,86 @@
+#Mixup-generator
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.utils.data_utils import Sequence
+
+class MixupImageDataGenerator():
+    def __init__(self, generator, directory, batch_size, img_height, img_width, distr, params):
+
+        self.batch_index = 0
+        self.batch_size = batch_size
+        self.params = params
+        self.distr = distr
+        self.shape = (img_height, img_width)
+        # First iterator yielding tuples of (x, y)
+        self.generator1 = generator.flow_from_directory(directory,
+                                                        target_size=(
+                                                            img_height, img_width),
+                                                        class_mode="categorical",
+                                                        batch_size=batch_size,
+                                                        shuffle=True)
+
+        # Second iterator yielding tuples of (x, y)
+        self.generator2 = generator.flow_from_directory(directory,
+                                                        target_size=(
+                                                            img_height, img_width),
+                                                        class_mode="categorical",
+                                                        batch_size=batch_size,
+                                                        shuffle=True)
+
+        # Number of images across all classes in image directory.
+        self.n = self.generator1.samples
+
+    def reset_index(self):
+
+        self.generator1._set_index_array()
+        self.generator2._set_index_array()
+
+    def on_epoch_end(self):
+        self.reset_index()
+
+    def reset(self):
+        self.batch_index = 0
+
+    def __len__(self):
+        # round up
+        return (self.n + self.batch_size - 1) // self.batch_size
+
+    def get_steps_per_epoch(self):
+        return self.n // self.batch_size
+
+    def __next__(self):
+
+        if self.batch_index == 0:
+            self.reset_index()
+
+        current_index = (self.batch_index * self.batch_size) % self.n
+        if self.n > current_index + self.batch_size:
+            self.batch_index += 1
+        else:
+            self.batch_index = 0
+
+        # random sample the lambda value from beta distribution.
+        if self.distr == "beta":
+            l = np.random.beta(self.params, self.params, self.batch_size)
+		
+		#May add other distributions
+
+        X_l = l.reshape(self.batch_size, 1, 1, 1)
+        y_l = l.reshape(self.batch_size, 1)
+
+        # Get a pair of inputs and outputs from two iterators.
+        X1, y1 = self.generator1.next()
+        X2, y2 = self.generator2.next()
+
+        # Perform the mixup.
+        X = X1 * X_l + X2 * (1 - X_l)
+        y = y1 * y_l + y2 * (1 - y_l)
+        return (X, y)
+
+    def __iter__(self):
+        while True:
+            yield next(self)
--- testmodel.py
+++ testmodel.py
@@ -3,58 +3,122 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+import mixupGenerator as mixupgen
+import matplotlib.pyplot as plt
 
-inputShape = (150,150) #using 150x150 for testmodel but actual data is 224x224
+
+IMG_HEIGHT = 224
+IMG_WIDTH = 224 
+epochs = 2
+batch_size  = 10
 
 trainDirectory = "../Data/train"
 validationDirectory = "../Data/valid"
 testDirectory = "../Data/test"
 
+classes = os.listdir(trainDirectory)
+num_classes = len(classes)
+
 trainDataGen = ImageDataGenerator(rescale = 1./255.) #rescale as in previous assignment
 validDataGen = ImageDataGenerator(rescale = 1./255.) 
 testDataGen = ImageDataGenerator(rescale = 1./255.)
 
-#25812 img belonging to 190 classes
-trainGen = trainDataGen.flow_from_directory(trainDirectory,
-											batch_size = 10,
-											class_mode = "categorical",
-											target_size = inputShape) 
+#with mixup
+trainGen = mixupgen.MixupImageDataGenerator(trainDataGen, 
+											trainDirectory,
+											batch_size = batch_size,
+											img_height=IMG_HEIGHT,
+											img_width=IMG_WIDTH,
+											distr = "beta",
+											params = 0.2)
+
+
+
+
+#trainGen = trainDataGen.flow_from_directory(trainDirectory,
+											#batch_size = batch_size,
+											#class_mode = "categorical",
+											#target_size = (IMG_HEIGHT, IMG_WIDTH)) 
 
 #950 img belonging to 190 classes
 validGen = validDataGen.flow_from_directory(validationDirectory,
-											batch_size = 10,
+											batch_size = batch_size,
 											class_mode = "categorical",
-											target_size = inputShape) 
+											target_size = (IMG_HEIGHT, IMG_WIDTH)) 
 
 #950 img belonging to 190 classes
 testGen = testDataGen.flow_from_directory(testDirectory,
-											batch_size = 10,
+											batch_size = batch_size,
 											class_mode = "categorical",
-											target_size = inputShape) 
+											target_size = (IMG_HEIGHT, IMG_WIDTH)) 
 
 
-testModel = tf.ḱeras.models.Sequential([
-	tf.python.fkeras.layers.Conv2D(16, (3,3), activation = "relu", input_shape = inputShape),
-	tf.keras.layers.MaxPooling2D(2,2),
-	tf.keras.layers.Conv2D(32, (3,3), activation = "relu"),
-	tf.keras.layers.MaxPooling2D(2,2),
-	tf.keras.layers.Conv2D(64, (3,3), activation = "relu"),
-	tf.keras.layers.MaxPooling2D(2,2),
-	tf.keras.layers.Flatten(),
-	tf.keras.layers(1024, activation = "relu"),
-	tf.keras.layers.Dense(1, activation = "softmax")
+testModel = Sequential([
+	Conv2D(16, 3, activation = "relu", input_shape = (IMG_HEIGHT, IMG_WIDTH, 3)),
+	MaxPooling2D(2,2),
+	Conv2D(32, 3, activation = "relu"),
+	MaxPooling2D(2,2),
+	Conv2D(64, 3, activation = "relu"),
+	MaxPooling2D(2,2),
+	Flatten(),
+	Dense(1024, activation = "relu"),
+	Dense(num_classes, activation = "softmax") #Need 190 since we have 190 classes
 	])
 
 testModel.compile(optimizer = RMSprop(lr = 0.001),
 				  loss = "categorical_crossentropy",
 				  metrics = ["acc"])
 
+
+testModel.summary()
+
+
+###################################################################################
+#this is an example on how to plot a batch of images in the training data
+(X, y) = next(trainGen)
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(10,10))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+plotImages(X)
+###################################################################################
+
+
+
 hist = testModel.fit_generator(trainGen,
-							   validGen,
-							   steps_per_epoch = 100,
-							   epochs = 10,
-							   validation_steps = 50,
+							   steps_per_epoch = trainGen.n//batch_size, #training images / batch size
+							   epochs = 2,
+							   validation_data = validGen,
+							   validation_steps = 95,
 							   verbose = 1)
 
+#acc = history.history['accuracy']
+#val_acc = history.history['val_accuracy']
 
+#loss=history.history['loss']
+#val_loss=history.history['val_loss']
+
+#epochs_range = range(epochs)
+
+#plt.figure(figsize=(8, 8))
+#plt.subplot(1, 2, 1)
+#plt.plot(epochs_range, acc, label='Training Accuracy')
+#plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+#plt.legend(loc='lower right')
+#plt.title('Training and Validation Accuracy')
+
+#plt.subplot(1, 2, 2)
+#plt.plot(epochs_range, loss, label='Training Loss')
+#plt.plot(epochs_range, val_loss, label='Validation Loss')
+#plt.legend(loc='upper right')
+#plt.title('Training and Validation Loss')
+#plt.show()
 
